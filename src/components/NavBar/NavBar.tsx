15d5c29cde6b82518ea0c7fcab1e7130dd28ff4d
Dayib OSMAN <dayibosman77@gmail.com>
2026-06-23 10:34:13 +0300

    Old commits.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
-      {/* <Heading fontSize="2xl">GameHub</Heading> */}
+      {/* <Heading >GameHub</Heading> */}
       <DarkModeToggle />
     </HStack>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider";
+import { Provider } from "./components/ui/provider"; // The generated provider
 
 import App from "./App.tsx";
 
