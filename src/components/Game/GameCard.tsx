91b32f2a222d181e3dcd60eb7edd63c4fc845c20
Dayib OSMAN <dayibosman77@gmail.com>
2026-06-24 12:51:32 +0300

    Get Optimized images.

--- src/components/Game/GameCard.tsx
+++ src/components/Game/GameCard.tsx
@@ -2,6 +2,7 @@ import type { Game } from "@/hooks/useGames";
 import { Card, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 interface Props {
   game: Game;
@@ -10,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card.Root maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Image src={game.background_image} alt={game.name} />
+      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <Card.Body padding="4">
         <Heading as="h3" size="md">
           {game.name}
---
+++ src/services/image-url.ts
@@ -0,0 +1,10 @@
+const getCroppedImageUrl = (imageUrl: string): string => {
+  if (!imageUrl) return ""; // Return an empty string if imageUrl is undefined or null
+
+  const targetSubstring = "media/";
+  const index = imageUrl.indexOf(targetSubstring) + targetSubstring.length; // Find the index of the target substring and add its length to get the starting index of the file name
+
+  return imageUrl.slice(0, index) + "crop/600/400/" + imageUrl.slice(index); // Construct the new URL by slicing the original URL and inserting the crop parameters
+};
+
+export default getCroppedImageUrl;
