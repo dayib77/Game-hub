ad9cea0416e893f15a89d8f3d27b18edb3489678
Dayib OSMAN <dayibosman77@gmail.com>
2026-08-17 18:20:56 +0300

    Improving layout.

--- src/components/Game/GameDetailPage.tsx
+++ src/components/Game/GameDetailPage.tsx
@@ -1,5 +1,5 @@
 import useGameDetail from "@/hooks/useGameDetail";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router";
 
 import ExpandableText from "../ExpandableText";
@@ -15,13 +15,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.descript_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshot gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} gap={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.descript_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshot gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
